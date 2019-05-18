765150dbec770ef6414c277dafb769acc3690c68
patiboh <lapatiboh@gmail.com>
2019-05-18 16:32:44 +0200

    WIP - try loading an image

--- src/xp/images.js
+++ src/xp/images.js
@@ -1,50 +1,20 @@
 import * as THREE from 'three';
 
-let container;
-let camera;
-let scene;
-let renderer;
-let uniforms;
+export const geometry = new THREE.PlaneGeometry(10, 10 * 0.75);
 
-function render() {
-  uniforms.u_time.value += 0.05;
-  renderer.render(scene, camera);
-}
+export const uniforms = {
+  u_time: { type: 'f', value: 1.0 },
+  u_resolution: { type: 'v2', value: new THREE.Vector2() },
+  u_mouse: { type: 'v2', value: new THREE.Vector2() },
+};
 
-// eslint-disable-next-line no-unused-vars
-function onWindowResize(event) {
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  uniforms.u_resolution.value.x = renderer.domElement.width;
-  uniforms.u_resolution.value.y = renderer.domElement.height;
-}
+// Load an image file into a custom material
+const loader = new THREE.TextureLoader();
+const imagesMaterial = () => new THREE.MeshLambertMaterial({
+  map: loader.load('./static/images/tupac.jpg'),
+});
 
-function init() {
-  scene = new THREE.Scene();
-
-  /**
-  * Camera
-  * */
-
-  let geometry = new THREE.PlaneBufferGeometry(2, 2);
-  uniforms = {
-    u_time: { type: 'f', value: 1.0 },
-    u_resolution: { type: 'v2', value: new THREE.Vector2() },
-    u_mouse: { type: 'v2', value: new THREE.Vector2() },
-  };
-
-  geometry = new THREE.PlaneGeometry(10, 10 * 0.75);
-  const loader = new THREE.TextureLoader();
-  // Load an image file into a custom material
-  const image = new THREE.MeshLambertMaterial({
-    map: loader.load('./static/images/tupac.jpg'),
-  });
-
-  // const material = new THREE.ShaderMaterial({
-  //   uniforms,
-  //   vertexShader: vert,
-  //   fragmentShader: frag,
-  // });
-
+const imagesCamera = () => {
   // Specify the portion of the scene visiable at any time (in degrees)
   const fieldOfView = 75;
 
@@ -58,46 +28,25 @@ function init() {
   const nearPlane = 0.1;
   const farPlane = 1000;
 
-  camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
+  const cam = new THREE.PerspectiveCamera(
+    fieldOfView,
+    aspectRatio,
+    nearPlane,
+    farPlane,
+  );
   // Finally, set the camera's position in the z-dimension
-  camera.position.z = 5;
+  cam.position.z = 5;
+  return cam;
+};
 
-
-  const mesh = new THREE.Mesh(geometry, image);
-  scene.add(mesh);
-
-
-  // Add a point light with #fff color, .7 intensity, and 0 distance
-  const light = new THREE.PointLight(0xffffff, 1, 0);
-
+// Add a point light with #fff color, .7 intensity, and 0 distance
+const light = () => {
+  const pointLight = new THREE.PointLight(0xffffff, 1, 0);
   // Specify the light's position
-  light.position.set(1, 1, 100);
-  scene.add(light);
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
+  pointLight.position.set(1, 1, 100);
+  return pointLight;
+};
 
-
-  document.title = '👾 ✨ 🐳';
-  container = document.getElementById('root');
-  if (module.hot) {
-    module.hot.accept();
-  }
-
-  container.appendChild(renderer.domElement);
-
-  onWindowResize();
-  window.addEventListener('resize', onWindowResize, false);
-
-  // document.onmousemove = (e) => {
-  //   uniforms.u_mouse.value.x = e.pageX;
-  //   uniforms.u_mouse.value.y = e.pageY;
-  // };
-}
-
-function animate() {
-  requestAnimationFrame(animate);
-  render();
-}
-
-init();
-animate();
+export const material = imagesMaterial();
+export const camera = imagesCamera();
+export const pointLight = light();
